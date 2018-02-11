3f8d9ee1cb2b26e04e8ed07e74a76232d09fbaf0
kaija <kaija.chang@gmail.com>
2018-02-11 12:20:03 +0800

    Add IP block list library

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 var express = require('express');
+const ipbl = require('./ipbl');
+
+ipbl.setkey('xxxxxxxx');
+
+
 var router = express.Router();
+
+ipbl.ipreputation('185.174.137.249').then( (res) => {
+console.log(res);
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
