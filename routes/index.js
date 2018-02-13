97f86012ba4a536ed9dda45c706702a68bdf67f4
kaija <kaija.chang@gmail.com>
2018-02-13 17:27:25 +0800

    fixed query not found case

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,26 @@
 var express = require('express');
 const ipbl = require('./ipbl');
 
-ipbl.setkey('xxxxxxxx');
+ipbl.setkey('xxxxxxxxxx');
 
 
 var router = express.Router();
-
+/*
 ipbl.ipreputation('185.174.137.249').then( (res) => {
 console.log(res);
 });
 
+ipbl.lookup('www.google.com').then( (res) => {
+console.log(res);
+});
+
+ipbl.fqdnipreputation('www.google.com').then( (res) => {
+console.log(res);
+}).catch((err)=>{
+console.log(err);
+});
+
+*/
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
--- routes/ipbl.js
+++ routes/ipbl.js
@@ -5,8 +5,6 @@ resolv = new cares.Resolver({
     "8.8.8.8"
   ]
 });
-
-
 resolv_opt = {
   type: cares.NS_T_A,
   class: cares.NS_C_IN,
@@ -21,54 +19,97 @@ exports.setkey = function (key) {
 
 function ipdata(ans) {
   var data = {};
-  if (ans[0] != '127') {
-    data.err = 'result data format mismatch.'
+
+  if (ans[0] != "127") {
+    data.err = "result data format mismatch."
   } else {
     data.day = ans[1];
     data.score = ans[2];
+
     //more fast by using switch case
     switch (ans[3]) {
-      case '0':
-        data.type = ['Search Engine'];
+      case "0":
+        data.type = [
+          "Search Engine"
+        ];
         break;
-      case '1':
-        data.type = ['Suspicious'];
+
+      case "1":
+        data.type = [
+          "Suspicious"
+        ];
         break;
-      case '2':
-        data.type = ['Harvester'];
+
+      case "2":
+        data.type = [
+          "Harvester"
+        ];
         break;
-      case '3':
-        data.type = ['Suspicious', 'Harvester'];
+
+      case "3":
+        data.type = [
+          "Suspicious",
+          "Harvester"
+        ];
         break;
-      case '4':
-        data.type = ['Comment Spammer'];
+
+      case "4":
+        data.type = [
+          "Comment Spammer"
+        ];
         break;
-      case '5':
-        data.type = ['Suspicious', 'Comment Spammer'];
+
+      case "5":
+        data.type = [
+          "Suspicious",
+          "Comment Spammer"
+        ];
         break;
-      case '6':
-        data.type = ['Harvester', 'Comment Spammer'];
+
+      case "6":
+        data.type = [
+          "Harvester",
+          "Comment Spammer"
+        ];
         break;
-      case '7':
-        data.type = ['Suspicious', 'Harvester', 'Comment Spammer'];
+
+      case "7":
+        data.type = [
+          "Suspicious",
+          "Harvester",
+          "Comment Spammer"
+        ];
         break;
+
+      case "255":
+        data.type = [
+        ];
+        break;
+
       default:
-        data.type = ['Unknown'];
+        data.type = [
+          "Unknown"
+        ];
         break;
     }
   }
+
   return data;
 }
 
 function ipreputation(ip) {
   var ipb = ip.split(".").reverse().join(".");
+
   var fqdn = accesskey + "." + ipb + ".dnsbl.httpbl.org";  //console.log(fqdn);
+  console.log(fqdn);
+
   return new Promise(function (fulfill, reject) {
     lookup(fqdn).then((res) => {
-      ans = res.answer[0].address.split('.');
+      ans = res.answer[0].address.split(".");
       fulfill(ipdata(ans));
     }).catch((err) => {
-      reject(err);
+      ans = ['127', '0', '0', '255'];
+      fulfill(ipdata(ans));
     });
   });
 }
@@ -83,5 +124,20 @@ function lookup(fqdn) {
   });
 }
 
+function fqdnipreputation(fqdn) {
+  return new Promise(function (fulfill, reject) {
+    lookup(fqdn).then((res) => {
+      ipreputation(res.answer[0].address).then((res) => {
+        fulfill(res);
+      }).catch((err) => {
+        reject(err);
+      });
+    }).catch((err) => {
+      reject(err);
+    });
+  });
+}
+
 exports.lookup = lookup;
 exports.ipreputation = ipreputation;
+exports.fqdnipreputation = fqdnipreputation;
